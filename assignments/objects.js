d8e6cf0b6fc93ec5eac3f8b6cc6ed1a25d02072e
Leighton Fritze <Jay.Row.Fritze@gmail.com>
2019-08-19 11:51:20 -0400

    completed challenge 2 and started challenge 3

--- assignments/objects.js
+++ assignments/objects.js
@@ -63,16 +63,22 @@ console.log(mitzi.name);
 // Kennan's ID
 console.log(kennan.id);
 
-
 // Keven's email
+console.log(keven.email);
 
 // Gannie's name
+console.log(gannie.name);
 
 // Antonietta's Gender
+console.log(antonietta.gender);
 
 // ==== Challenge 3: Object Methods ==== 
 // Give Kennan the ability to say "Hello, my name is Kennan!" Use the console.log provided as a hint.
 // console.log(kennan.speak());
+kennan.speak = () => {
+  return `Hello, my name is ${kennan.name}!`; //make function//
+};
+console.log(kennan.speak());
 
 // Antonietta loves math, give her the ability to multiply two numbers together and return the product. Use the console.log provided as a hint.
 //console.log(antonietta.multiplyNums(3,4));
